a315275cc0ad936121be13ae9f16590821c0b39d
javidahmed64592 <javidahmed64592@github.com>
2021-11-02 21:15:51 +0000

    Added ability to specify axis from which to view the 2D projection.

--- Cube3D.py
+++ Cube3D.py
@@ -2,6 +2,8 @@ import numpy as np
 import matplotlib.pyplot as plt
 from mpl_toolkits.mplot3d import Axes3D
 
+plt.style.use('dark_background')
+
 class cube3D:
     """
     This class creates a cube from a specified width, theta which rotates around the z-axis,
@@ -9,11 +11,15 @@ class cube3D:
     of ones. The cube is projected onto a 2D plane, and both the cube and the projection can
     then be plotted. The rotated cube can also be plotted currently as a 3D scatter plot.
     """
-    def __init__(self, width, theta=0, phi=0):
+    def __init__(self, width, theta=0, phi=0, view_axis="z"):
         self.width = width
         self.theta = theta
         self.phi = phi
         self.name = "Cube_W%s_T%s_P%s" % (self.width, int(self.theta * 180 / np.pi), int(self.phi * 180 / np.pi))
+
+        self.view_axis = view_axis
+        self.axes = ["x", "y", "z"]
+
         self.x0 = np.arange(self.width)
         self.matrix = np.ones((self.width, self.width, self.width))
 
@@ -72,7 +78,7 @@ class cube3D:
                         theta = np.pi/2
 
                     if r != 0:
-                        phi = np.arcsin(np.sqrt(x**2 + y**2)/r)
+                        phi = np.arccos(z/r)
                     else:
                         phi = -np.pi/2
 
@@ -81,13 +87,17 @@ class cube3D:
                     self.coords.append(xi)
 
         self.XYZ = np.array(self.coords)
+        print(self.XYZ)
 
     def projection2D(self):
         """
         Projects 3D shape onto 2D plane.
         """
-        self.XY = np.delete(self.XYZ, -1, axis=1) # Remove the z coordinates
-        self.XY = np.unique(self.XY, axis = 0) # Remove duplicate xy coordinate pairs.
+        self.coords2D = np.delete(self.XYZ, self.axes.index(self.view_axis), axis=1) # Remove the z coordinates
+        self.coords2D = np.unique(self.coords2D, axis = 0) # Remove duplicate xy coordinate pairs.
+        temp_coords = self.axes
+        temp_coords.remove(self.view_axis)
+        self.axes2D = temp_coords
 
     def plot(self):
         """
@@ -97,7 +107,7 @@ class cube3D:
         idx = 1
 
         ax = fig.add_subplot(3, 3, idx, projection=Axes3D.name)
-        ax.voxels(self.matrix)
+        ax.voxels(self.matrix, facecolors="white")
         ax.set_title("3D: %s" % self.name)
         ax.set_xlabel('x')
         ax.set_ylabel('y')
@@ -105,14 +115,14 @@ class cube3D:
         idx += 1
 
         ax = fig.add_subplot(3, 3, idx)
-        ax.scatter(self.XY[:,0], self.XY[:,1], edgecolors='b')
+        ax.scatter(self.coords2D[:,0], self.coords2D[:,1], c="white")
         ax.set_title("2D Projection: %s" % self.name)
-        ax.set_xlabel('x')
-        ax.set_ylabel('y')
+        ax.set_xlabel(self.axes2D[0])
+        ax.set_ylabel(self.axes2D[1])
         idx += 1
 
         ax = fig.add_subplot(3, 3, idx, projection=Axes3D.name)
-        ax.scatter(self.XYZ[:,0], self.XYZ[:,1], self.XYZ[:,2])
+        ax.scatter(self.XYZ[:,0], self.XYZ[:,1], self.XYZ[:,2], c="white")
         ax.set_title("3D Scatter: %s" % self.name)
         ax.set_xlabel('x')
         ax.set_ylabel('y')
@@ -124,16 +134,16 @@ class cube3D:
 #cube1 = cube3D(4, theta=45*(np.pi/180))
 #cube1.plot()
 
-cube2 = cube3D(3, phi=0*(np.pi/180))
-cube2.plot()
+#cube2 = cube3D(8, theta=0*(np.pi/180))
+#cube2.plot()
 
-cube3 = cube3D(3, phi=15*(np.pi/180))
+cube3 = cube3D(8, theta=15*(np.pi/180), view_axis = "x")
 cube3.plot()
 
-cube4 = cube3D(3, phi=30*(np.pi/180))
+cube4 = cube3D(8, theta=15*(np.pi/180), view_axis = "y")
 cube4.plot()
 
-cube5 = cube3D(3, phi=45*(np.pi/180))
+cube5 = cube3D(8, theta=15*(np.pi/180), view_axis = "z")
 cube5.plot()
 
 plt.show()
