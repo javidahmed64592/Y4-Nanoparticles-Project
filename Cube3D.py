4020f206ea8100209efced79a5a6e1e7d9abca84
javidahmed64592 <javidahmed64592@github.com>
2021-11-09 05:04:23 +0000

    Added lattice constant and intensity to the plot. Also centred the cube around the origin

--- Cube3D.py
+++ Cube3D.py
@@ -6,9 +6,9 @@ plt.style.use("dark_background")
 
 class shape3D:
     """
-    This class creates a cube from a specified width and 3 angles of rotation, rx, ry, rz.
-    This cube can be represented as a 3D matrix comprised of ones. The cube is projected onto
-    a 2D plane, and both the cube and the projection can then be plotted. The rotated cube can
+    This class creates a shape from a specified width and 3 angles of rotation, rx, ry, rz.
+    This shape can be represented as a 3D matrix comprised of ones. The shape is projected onto
+    a 2D plane, and both the shape and the projection can then be plotted. The rotated shape can
     also be plotted currently as a 3D scatter plot.
     """
     def __init__(self, width=10, rx=0, ry=0, rz=0, view_axis="z"):
@@ -73,11 +73,17 @@ class shape3D:
         """
         Projects 3D shape onto 2D plane.
         """
+        # TODO: Change this to have greater intensity for duplicate coordinate pairs instead of removing them.
         self.coords2D = np.delete(self.XYZ, self.axes.index(self.view_axis), axis=1) # Remove the view axis coordinates
-        self.coords2D = np.unique(self.coords2D, axis = 0) # Remove duplicate coordinate pairs.
-        temp_coords = self.axes
-        temp_coords.remove(self.view_axis)
-        self.axes2D = temp_coords
+        self.coords2D, self.intensity = np.unique(np.around(self.coords2D, 2), axis = 0, return_counts = True) # Remove duplicate coordinate pairs.
+        temp_axes = self.axes
+        temp_axes.remove(self.view_axis)
+        self.axes2D = temp_axes
+
+        max = np.amax(self.intensity)
+        self.intensity = 0.4 + 0.5*(self.intensity/max) # Base intensity + (Scaling factor * Ratio of intensity to max intensity)
+        self.coords2D = self.coords2D[self.intensity.argsort()] # Sorting coordinates and corresponding intensities
+        self.intensity = np.array(["".join(item) for item in self.intensity[self.intensity.argsort()].astype(str)])
 
     def plot(self):
         """
@@ -95,10 +101,15 @@ class shape3D:
         idx += 1
 
         ax = fig.add_subplot(3, 3, idx)
-        ax.scatter(self.coords2D[:,0], self.coords2D[:,1], s=100, c="white")
+        ax.scatter(self.coords2D[:,0], self.coords2D[:,1], s=10000*np.pi / self.width**2, c=self.intensity)
         ax.set_title("2D Projection: %s" % self.name)
         ax.set_xlabel(self.axes2D[0])
         ax.set_ylabel(self.axes2D[1])
+
+        lims = [np.amin(self.coords2D) - 1, np.amax(self.coords2D) + 1]
+        ax.set_xlim(lims)
+        ax.set_ylim(lims)
+        ax.set_aspect('equal', adjustable='box')
         idx += 1
 
         ax = fig.add_subplot(3, 3, idx, projection=Axes3D.name)
@@ -112,15 +123,22 @@ class shape3D:
 
 class cube3D(shape3D):
     """
-    This class inherits from the shape3D cube and generates a name, x0, matrix and coords for a cube.
+    This class inherits from the shape3D class and generates a name, x0, matrix and coords for a cube.
     """
+    def __init__(self, width=10, rx=0, ry=0, rz=0, view_axis="z", a=1):
+        """
+        Introduces an additional parameter, a, which is the lattice constant.
+        """
+        self.a = a
+        super().__init__(width, rx, ry, rz, view_axis)
+
     def generate_shape(self):
         """
         Generates the name and initial coordinates for a cube.
         """
         self.name = "Cube_W%s_rx%s_ry%s_rz%s" % (self.width, int(self.rx * 180 / np.pi), int(self.ry * 180 / np.pi), int(self.rz * 180 / np.pi))
 
-        self.x0 = np.arange(self.width)
+        self.x0 = self.a * np.arange(-(self.width-1)/2, self.width/2, 1)
         self.matrix = np.ones((self.width, self.width, self.width))
 
         self.coords = []
@@ -130,8 +148,8 @@ class cube3D(shape3D):
                 for z in self.x0:
                     self.coords.append([x, y, z])
 
-# Examples with 8x8x8 cube
-cube1 = cube3D(8, rx=np.deg2rad(45), ry=np.deg2rad(45))
+# Examples with 10x10x10 cube
+cube1 = cube3D(10, rx=np.deg2rad(0), ry=np.deg2rad(45), a=1)
 cube1.plot()
 
 plt.show()
