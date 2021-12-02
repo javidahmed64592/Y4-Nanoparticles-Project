a2573cdde4a521e5f6f5a29a36e18660f0b26a03
javidahmed64592 <javidahmed64592@github.com>
2021-12-02 23:15:46 +0000

    Added blur directly into Shape3D class

--- Generate_Data.py
+++ Generate_Data.py
@@ -3,26 +3,21 @@ import numpy as np
 from Shape3D import cube3D
 import datetime
 
-def generate_cube(width, rx, ry, a, defect, pos_error, save_path=os.path.join(os.getcwd(), "Simulated Data"), file_type=".png", iter=0):
-    """
-    Create cube of specified properties and save the projection.
-    """
-    cube = cube3D(width, rx, ry, a=a, defect=defect, pos_error=pos_error)
-    cube.save_projection(save_path=save_path, file_type=file_type, iter=iter)
-
 if __name__ == "__main__":
     begin_time = datetime.datetime.now()
 
     # Properties of the cube and its projection
     file_type = "png"
     training_path = os.path.join(os.getcwd(), "Machine Learning", "Training Data")
-    save_path = os.path.join(training_path, "Test Originals 7") # Change to specify where to save
+    save_path = os.path.join(training_path, "Originals 3") # Change to specify where to save
 
     width = 10
-    a = 0.7
-    rx = [0, 10, 20, 30] # Angle in degrees
+    a = 0.8
+    rx = [0] # Angle in degrees
     ry = [0]
-    d = [0.07*i for i in range(3)] # Percentage defect of the shape
+    d = [0.02*i for i in range(1)] # Percentage defect of the shape
+
+    blur = None # Strength of Gaussian blur
 
     iters = 1 # How many of each cube to make
 
@@ -34,8 +29,9 @@ if __name__ == "__main__":
             for angle_x in rx:
                 for defect in d:
                     for i in range(iters):
-                        generate_cube(width, angle_x, angle_y, a, defect, np.random.uniform(0.06, 0.09), save_path, file_type, i)
                         print("Generating cube %s of %s." % (counter, total_cubes))
+                        cube = cube3D(10, angle_x, angle_y, a=a, defect=defect, pos_error=np.random.uniform(0.04, 0.07))
+                        cube.save_projection(save_path=save_path, file_type=file_type, blur=blur, iter=i)
                         counter += 1
 
     print("Done! The time it took is %s seconds." % round((datetime.datetime.now() - begin_time).total_seconds(), 2))
--- Shape3D.py
+++ Shape3D.py
@@ -1,11 +1,13 @@
 import numpy as np
 import os
+import cv2
 
 import matplotlib.pyplot as plt
-from mpl_toolkits.mplot3d import Axes3D
 
 plt.style.use("dark_background")
+plt.axis("tight")
 fig = plt.figure(figsize=(3, 3))
+
 
 class shape3D:
     """
@@ -14,13 +16,12 @@ class shape3D:
     a 2D plane, and both the shape and the projection can then be plotted. The rotated shape can
     also be plotted currently as a 3D scatter plot.
     """
-    def __init__(self, width=10, rx=0, ry=0, rz=0, view_axis="z", defect=0, pos_error=0):
+    def __init__(self, width=10, rx=0, ry=0, rz=0, a=1, defect=0, pos_error=0):
         """
         Initialises the class.
         Inputs:
             width: Integer, width of the shape being created
             rx, ry, rz: Float, rotations in degrees about the x, y, z axes respectively
-            view_axis: String, axis from which to view the 2D projection of the 3D shape
             defect: Float between 0 and 1, percentage of defects in the shape, 0 is no defects
             pos_error: Float, error in the position of each point
         """
@@ -28,12 +29,9 @@ class shape3D:
         self.rx = rx
         self.ry = ry
         self.rz = rz
-
-        self.view_axis = view_axis
-        self.axes = ["x", "y", "z"]
-
         self.defect = defect
         self.pos_error = pos_error
+        self.a = a
 
         self.generate_shape()
         self.rotation_matrices()
@@ -54,7 +52,6 @@ class shape3D:
         ry = np.deg2rad(self.ry)
         rz = np.deg2rad(self.rz)
 
-
         Rx = np.array([[           1,           0,           0],
                        [           0,  np.cos(rx), -np.sin(rx)],
                        [           0,  np.sin(rx),  np.cos(rx)]])
@@ -73,46 +70,52 @@ class shape3D:
         """
         Generates all xyz coordinates.
         """
-        v = []
-
-        for u in self.coords:
-            v.append(np.matmul(self.R, np.array(u)))
-
-        self.XYZ = np.array(v)
+        x0 = self.a * np.arange(-(self.width-1)/2, self.width/2, 1)
+        coords = np.reshape(np.meshgrid(x0, x0, x0), (3, -1))
+        coords += np.random.normal(0, self.pos_error, np.shape(coords)) * self.a
+        coords = np.matmul(self.R, coords)
+        self.coords2D = coords[0:, :]
 
     def projection2D(self):
         """
         Projects 3D shape onto 2D plane.
         """
-        self.coords2D = np.delete(self.XYZ, self.axes.index(self.view_axis), axis=1) # Remove the view axis coordinates
+        coords2D_shape = np.shape(self.coords2D)
 
+        # Defect
         if self.defect != 0:
-            iters = int(self.defect * np.shape(self.coords2D)[0])
+            iters = int(self.defect * coords2D_shape[0])
 
             for _ in range(iters):
-                self.coords2D = np.delete(self.coords2D, np.random.randint(np.shape(self.coords2D)[0]-1), 0)
+                self.coords2D = np.delete(self.coords2D, np.random.randint(coords2D_shape[0]-1), 0)
 
-        self.coords2D[:,0] += np.random.uniform(-1, 1)
-        self.coords2D[:,1] += np.random.uniform(-1, 1)
+        # Translate shape around randomly
+        self.coords2D[0, :] += np.random.uniform(-1, 1)
+        self.coords2D[1, :] += np.random.uniform(-1, 1)
 
+        # Creating the plot
         self.ax = fig.add_subplot(1, 1, 1)
+        self.ax.set_axis_off()
 
-        blurs = 30
-        start_size = 300
-        blur_res = 2000
-
-        for blur in range(blurs): # Artificial blur around points
-            alpha = 2 / (blur**2+0.5)
-            size = start_size + (blur_res * blur)
-            self.ax.scatter(self.coords2D[:,0], self.coords2D[:,1], s=size * (np.pi / self.width**2), c="white", alpha=alpha/blurs, edgecolors="none")
-
-        lims = [-self.width/2 - 2, self.width/2 + 2]
+        outer = 2
+        lims = [-self.width/2 - outer, self.width/2 + outer]
         self.ax.set_xlim(lims)
         self.ax.set_ylim(lims)
         self.ax.set_aspect('equal', adjustable='box')
-        self.ax.set_axis_off()
 
-    def save_projection(self, save_path=os.path.join(os.getcwd(), "Simulated Data"), file_name = "default", file_type=".svg", iter=0):
+        # Artificial blur around points
+        blurs = 25
+        blur = np.arange(blurs)
+        start_size = 5
+        blur_res = 50
+
+        alpha = 2 / ((blur)**2+(12*blur)+1) / self.width
+        size = start_size + (blur_res * blur)
+
+        for b in blur:
+            self.ax.scatter(self.coords2D[0, :], self.coords2D[1, :], s=size[b], c="white", alpha=alpha[b])
+
+    def save_projection(self, save_path=os.path.join(os.getcwd(), "Simulated Data"), file_name = "default", file_type=".png", blur=None, iter=0):
         """
         Saves the 2D projection.
         Inputs:
@@ -121,43 +124,35 @@ class shape3D:
             file_type: String, what file type to save the file as
             iter: Integer, number of cubes with same properties + 1
         """
-        directory = "W%s RX%s RY%s" % (self.width, self.rx, self.ry)
-        file_path = save_path # os.path.join(save_path, directory) instead to separate cubes into folders
-        if not os.path.exists(file_path):
-            os.makedirs(file_path)
+        if not os.path.exists(save_path):
+            os.makedirs(save_path)
 
         if file_name == "default":
-            file_name = directory + (" D%s %s.%s" % (int(100*self.defect), iter, file_type))
-        extent = self.ax.get_window_extent().transformed(fig.dpi_scale_trans.inverted())
-        fig.savefig(os.path.join(file_path, file_name), format=file_type, bbox_inches=extent)
+            file_name = self.name + (" %s.%s" % (iter, file_type))
+
+        fig.canvas.draw()
+        img = np.fromstring(fig.canvas.tostring_rgb(), dtype = np.uint8, sep = '')
+        img = img.reshape(fig.canvas.get_width_height()[::-1] + (3, ))
+        img = cv2.cvtColor(img, cv2.COLOR_RGB2BGR)
+        cv2.imwrite(os.path.join(save_path, file_name), img)
+
+        if blur is not None:
+            dst = cv2.GaussianBlur(img, (blur, blur), 0) # Blur image
+            blur_name = file_name.split(".")[0] + (" %s B%s.%s" % (iter, blur, file_type))
+            blur_path = os.path.join(save_path, "Blurred", "RX%s" % (self.rx))
+            if not os.path.exists(blur_path):
+                os.makedirs(blur_path)
+            cv2.imwrite(os.path.join(blur_path, blur_name), cv2.resize(dst, (128, 128)))
+
         fig.clf()
 
 class cube3D(shape3D):
     """
-    This class inherits from the shape3D class and generates a name, x0, matrix and coords for a cube.
+    This class inherits from the shape3D class and is specifically for a cube.
     """
-    def __init__(self, width=10, rx=0, ry=0, rz=0, view_axis="z", a=1, defect=0, pos_error=0):
+    def __init__(self, width=10, rx=0, ry=0, rz=0, a=1, defect=0, pos_error=0):
         """
-        Introduces an additional parameter, a, which is the lattice constant.
+        Create a cube and include that in the object name.
         """
-        self.a = a
-        super().__init__(width, rx, ry, rz, view_axis, defect, pos_error)
-
-    def generate_shape(self):
-        """
-        Generates the name and initial coordinates for a cube.
-        """
-        self.name = "Cube W%s RX%s RY%s" % (self.width, self.rx, self.ry)
-
-        self.x0 = self.a * np.arange(-(self.width-1)/2, self.width/2, 1)
-
-        self.coords = []
-
-        for x in self.x0:
-            for y in self.x0:
-                for z in self.x0:
-                    self.coords.append([x, y, z])
-        
-        self.coords = np.array(self.coords)
-        self.coords += np.random.normal(0, self.pos_error, np.shape(self.coords)) * self.a
-
+        super().__init__(width, rx, ry, rz, a, defect, pos_error)
+        self.name = "Cube W%s RX%s RY%s D%s" % (self.width, self.rx, self.ry, int(100*self.defect))
